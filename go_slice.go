ff008a888a2992f9250614e132240433d0ba0eb5
CZ <czavaleta@gbtrealty.com>
2018-10-05 22:56:28 -0500

    ficing some stuff

--- go_slice.go
+++ go_slice.go
@@ -1,9 +1,16 @@
 package main
 import (
 	"fmt"
+	"runtime"
 )
 
 func main() {
+	slicing()
+	slicing2()
+
+}
+
+func slicing() {
 	// here we use make to allocate memory for the underlying array and also
 	// init the slice, using make we can do both
 	originalPokedex := make([]string, 0, 150) // length, capacity
@@ -11,6 +18,27 @@ func main() {
 	originalPokedex = originalPokedex[0:150]
 	originalPokedex[141] = "Mew"
 	originalPokedex[149] = "Charizard"
+
 	fmt.Println(originalPokedex)
+
 }
+
+func slicing2() {
+	originalPokedex := make([]int, 0, 5) // length, capacity
+	c := cap(originalPokedex)
+	fmt.Println(c)
+
+	for i := 0; i <25; i++ {
+		originalPokedex = append(originalPokedex, i)
+
+		// if our capacity has changed
+		// Go had to grow to accomodate for the new data
+		if cap(originalPokedex) != c {
+			c = cap(originalPokedex)
+			fmt.Println(c)
+		}
+	}
+
+}
+
 
